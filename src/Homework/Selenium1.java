55ba9f5b55e679e3448dd607cd5dfc02174db1df
Olha <qwerty@mail.ru>
2020-05-18 15:29:13 +0300

    Houston, we have a problem

--- src/Homework/Selenium1.java
+++ src/Homework/Selenium1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rozetka.com.ua/");
